03645692903149d16d6e07d43588efee04b2e0f3
Kau-25 <98998544+Kau-25@users.noreply.github.com>
2026-03-01 06:04:02 -0800

    Added ultrasonic tool for gpt prompter to call

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -9,7 +9,7 @@ void setup() {
   Bridge.begin();
   Bridge.provide("show_listening", show_listening);
   Bridge.provide("show_message", show_message);
-  
+  Bridge.provide("show_feet", show_feet);
   Monitor.begin();
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -27,4 +27,8 @@ void show_listening() {
 
 void show_message(String msg) {
   oled_showMessage(msg);
+}
+
+float show_feet() {
+  return getUltrasonicSnapshotFeet();
 }
